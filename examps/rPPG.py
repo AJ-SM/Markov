8af7ce643ec33f9a33fa23a7df28384a9a13f983
AJ-SM <anujsidam1108@gmail.com>
2026-03-08 11:00:14 +0530

    added rppg

--- examps/rPPG.py
+++ examps/rPPG.py
@@ -1,41 +1,58 @@
 import cv2 as cv
-import mediapipe as mp 
-
-
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 # Path 
 path = r"D:\Storeage-1\Main\ML-Model\data_set\NiggaVideo.mp4"
 cap = cv.VideoCapture(path)
 
-# 1. Initialize MediaPipe Face Mesh
-mp_face_mesh = mp.solutions.face_mesh
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+# Load model
+base_options = python.BaseOptions(model_asset_path="./face_landmarker.task")
 
-# Initialize the model
-face_mesh = mp_face_mesh.FaceMesh(
-    max_num_faces=1,                # Limit to 1 face for better performance
-    refine_landmarks=True,          # Includes iris landmarks (478 total)
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
+options = vision.FaceLandmarkerOptions(
+    base_options=base_options,
+    output_face_blendshapes=True,
+    output_facial_transformation_matrixes=True,
+    num_faces=2
 )
 
-cv.namedWindow("NiggaVideo",cv.WINDOW_NORMAL)
-cv.resizeWindow("NiggaVideo",1080,720)
+detector = vision.FaceLandmarker.create_from_options(options)
 
 
+cv.namedWindow("NiggaVideo", cv.WINDOW_NORMAL)
+cv.resizeWindow("NiggaVideo", 1080, 720)
 
 while True:
-    ret,frame = cap.read()
-
-    embs = face_mesh.process(frame)
-    if not ret:
+    ret, frame = cap.read()
+    if not ret: 
         break
+    
+    # Convert the BGR frame to RGB for MediaPipe
+    rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    
+    # Process the frame
+    rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
-    cv.imshow("NiggaVideo",frame)
-    cv.imshow("NiggaVideo",embs)
+    mp_image = mp.Image(
+        image_format=mp.ImageFormat.SRGB,
+        data=rgb
+    )
 
-    if cv.waitKey(25) & 0xFF ==ord('q'):
+    result = detector.detect(mp_image)
+
+
+
+    if result.face_landmarks:
+        for face_landmarks in result.face_landmarks:
+            for landmark in face_landmarks:
+                x = int(landmark.x * frame.shape[1])
+                y = int(landmark.y * frame.shape[0])
+                cv.circle(frame, (x, y), 1, (0,255,0), -1)
+
+    cv.imshow("FaceMesh", frame)
+
+    if cv.waitKey(25) & 0xFF == ord('q'):
         break
 
 cap.release()
